c21805b19747cf8c4e92d8688768db904f67ceb4
MoreiraJorge <moreirajorge_17@outlook.pt>
2021-03-03 16:29:21 +0000

    Added typegraphql working route

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,51 @@ import Koa from "koa";
 import cors from "koa2-cors";
 import logger from "koa-logger";
 import chackStatus from "./routes/checkStatus"
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-koa";
+import { buildSchema, Resolver, Query } from "type-graphql";
 
 require("dotenv").config();
 
 const PORT = process.env.SERVER_PORT || 3001;
 const SERVER_HOST = process.env.LOCAL_HOST;
 
-const app = new Koa();
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello(){
+    return "Hello World";
+  }
+}
 
-app
-  .use(
-    cors({
-      origin: "*",
-    })
-  )
-  .use(logger())
-  .use(chackStatus.routes());
+const main = async () => {
 
-app
-  .listen(PORT, () => {
-    console.log(`Server running on http://${SERVER_HOST}:${PORT}`);
-  })
-  .on("error", (err) => {
-    console.log(err);
+  const schema = await buildSchema({
+    resolvers: [HelloResolver]
   });
+
+  const apolloServer = new ApolloServer({schema});
+
+  const app = new Koa();
+
+  apolloServer.applyMiddleware({ app });
+
+  app
+    .use(
+      cors({
+        origin: "*",
+      })
+    )
+    .use(logger())
+    .use(chackStatus.routes());
+
+  app
+    .listen(PORT, () => {
+      console.log(`Server running on http://${SERVER_HOST}:${PORT}`);
+    })
+    .on("error", (err) => {
+      console.log(err);
+    });
+}
+
+main();
